06c8b522a0a4cdeb04a3e7a82a3e79e9ebd88fc9
aleks <aleks@192.168.2.49>
2021-03-07 15:05:44 -0500

    Setting constructors right so I don't get any annoying errors later.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/CompPlayer.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/CompPlayer.java
@@ -5,15 +5,29 @@
  */
 package ca.sheridancollege.project.Players;
 
+//Imports:________________________________
+
+import java.util.*;
+import ca.sheridancollege.project.Cards.Card;
+
 /**
  *
- * @author aleks
+ * @author AllyCat13 @ Sheridan High 2021. 
  */
-public class CompPlayer extends Player{
+public class CompPlayer extends Player {
+
+    public CompPlayer() {
+        super();//For visual understanding.
+    }//End C:*
+
+    public CompPlayer(String name, ArrayList<Card> hand) {
+        super("Computer", hand);
+    }//End C:*
 
     @Override
     public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
     }//End M:*
-    
+
 }//End Class:______________________+

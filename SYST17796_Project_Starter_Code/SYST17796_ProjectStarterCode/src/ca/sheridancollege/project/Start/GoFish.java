347c48e92f1b13fec26c1cc0e894ae6cecbb5e69
aleks <aleks@192.168.2.49>
2021-03-07 13:48:41 -0500

    Creating game types and setting constructors.

---
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/GoFish.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project.Start;
+
+/**
+ *
+ * @author aleks
+ */
+public class GoFish extends Game{
+    
+    public GoFish(){
+    super("Go Fish");
+    }//End C:*
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }//End M:*
+
+    @Override
+    public void declareWinner() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }//End M:*
+    
+}//End Class:________________+

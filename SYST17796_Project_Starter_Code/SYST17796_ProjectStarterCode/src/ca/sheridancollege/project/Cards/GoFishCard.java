be750dba2773dd3fe973b4d930f593adfd3cb4b4
aleks <aleks@192.168.2.49>
2021-03-07 13:23:51 -0500

    Creating folders for organization.

---
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Cards/GoFishCard.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author aleks
+ */
+public class GoFishCard extends Card{
+    
+    public GoFishCard(){
+    super(Suit.DEFAULT, Value.DEFAULT);
+    }//End C:*
+    public GoFishCard(Suit suit, Value value){
+      super(suit, value);
+    }//End C:*
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}//End Class:__________________+

d567a45b3ee91ae33879a3c0986b302b1f3c181a
aleks <aleks@192.168.2.49>
2021-03-07 13:24:00 -0500

    Creating folders for organization.

---
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/CompPlayer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author aleks
+ */
+public class CompPlayer extends Player{
+
+    @Override
+    public void play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }//End M:*
+    
+}//End Class:______________________+

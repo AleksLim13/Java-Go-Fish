1fd30a592510bb562e7ee59eac40ad76d68aad7a
aleks <aleks@192.168.2.49>
2021-03-07 15:06:31 -0500

    Setting constructors right so I don't get any annoying errors later.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/Player.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/Player.java
@@ -1,5 +1,5 @@
 /**
- * SYST 17796 Project Winter 2019 Base code.
+ * SYST 17796 Project Winter 2021 Base code of a could be any game.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
  */
@@ -9,52 +9,50 @@ import ca.sheridancollege.project.Cards.Card;
 import java.util.*;
 
 /**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
+ * A class that models each Player in a possible any game. 
+ * Show expertise in generic programming. 
+ * Players have an identifier, which should be unique.
+ * Provide user manual like in Linux man pages. 
+ * Remember to do that for Java2 class as well. 
+ * Best OOP design win the free Surface Pro Laptop. 
+ * @author AllyCat13 @ Sheridan High 2021.
  */
 public abstract class Player 
 {
-    private String playerID; //the unique ID for this player
     
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
+    //Fields:_______________________
+   
+    protected String name;
+    protected ArrayList<Card> hand; 
+    protected static int numOfPlayers; //the unique ID for this player
+    protected int playerId;
+   
+    //Constructors:______________________________
     
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
+    public Player()
+    {  
+        name = "default";    
+        hand = new ArrayList<>();
+        numOfPlayers++;
+        playerId = numOfPlayers;
+    }//End C:*
+    
+    public Player(String name, ArrayList<Card> hand)
     {
-        return playerID;
+        this.name = "default";
+        this.hand = hand;
+        numOfPlayers++;
+        playerId = numOfPlayers;
+    }//End C:*
+    
+  
+    //Getters & Setters:________________________
+    
+    public int getPlayerID() 
+    {
+        return playerId;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
-        playerID = givenID;
-    }
-    
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
-    private String name;
-    private ArrayList<Card> hand; 
-    
-    public Player(){
-    
-    }
-    
     public String getName(){
     return name;
     }
@@ -70,5 +68,8 @@ public abstract class Player
     public void setHand(ArrayList<Card> newHand){
     this.hand = newHand; 
     }
+    
+    //Methods:_________________________
+    public abstract void play();
     
 }//End Class:_________________

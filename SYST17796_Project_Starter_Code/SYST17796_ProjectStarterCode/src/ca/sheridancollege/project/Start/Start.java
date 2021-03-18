3f4086b5a840cae7550192ace5c38b83d73d9a4d
aleks <aleks@192.168.2.49>
2021-03-18 14:50:11 -0400

    Fixing dependency injection by constructor.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/Start.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/Start.java
@@ -5,24 +5,52 @@
  */
 package ca.sheridancollege.project.Start;
 
+import ca.sheridancollege.project.Cards.Card;
 import ca.sheridancollege.project.Players.CompPlayer;
 import ca.sheridancollege.project.Players.HumanPlayer;
+import ca.sheridancollege.project.Players.Player;
 import ca.sheridancollege.project.Turns.Dealer;
 import ca.sheridancollege.project.Turns.TurnSwitcher;
+import ca.sheridancollege.project.Utility.UInput;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author AllyCat13 @ Sheridan High 2021. 
  */
 public class Start {
-    
+    /*
+    String name,
+                       ArrayList<Card> hand,
+                       HashMap<Card.Value, Integer> scoreBoard,
+                       HashMap<Card.Value, Integer> books,
+                       ArrayList<Card> desirableList
+    */
     public static void main(String[] args){
+    
+        Player human = new HumanPlayer(UInput.promptStringUser(),
+                                             new ArrayList<>(),
+                                             new HashMap<>(),
+                                             new HashMap<>(),
+                                             new ArrayList<>());
+        Player computer = new CompPlayer("Computer",
+                                             new ArrayList<>(),
+                                             new HashMap<>(),
+                                             new HashMap<>(),
+                                             new ArrayList<>());
     GoFish game = new GoFish(
                              new Dealer(),
-                             new HumanPlayer(),
-                             new CompPlayer(),
-                             new TurnSwitcher()
-                             );
+                             human,
+                             computer,
+                             new TurnSwitcher(human,
+                                              computer,
+                                              human,
+                                              computer,
+                                              new ArrayList<>(),
+                                              new ArrayList<>()
+                                               )//End inner:* 
+                            );//End outer:*
     game.play();
     }//End M:*
     

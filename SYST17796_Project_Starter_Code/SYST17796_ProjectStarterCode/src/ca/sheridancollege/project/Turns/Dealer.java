abc8124b216f944839c0d6820acc82db03b9c27d
aleks <aleks@192.168.2.49>
2021-03-19 19:56:15 -0400

    Clean up.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Turns/Dealer.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Turns/Dealer.java
@@ -11,7 +11,6 @@ import ca.sheridancollege.project.Cards.GoFishCard;
 import ca.sheridancollege.project.Players.Player;
 import ca.sheridancollege.project.Utility.UInput;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  *
@@ -92,73 +91,59 @@ public class Dealer {
     }//End M:*
 
     //public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, DEFAULT};
-    public HashMap<Card.Value, Integer> recordTypes(
+    public int[][] recordTypes(
             Card.Value card,
-            HashMap<Card.Value, Integer> scoreBoard
+            int[][] scoreBoard
     ) {
 
-        //A: 
-        Card.Value ACE = Card.Value.ACE;
-        Card.Value TWO = Card.Value.TWO;
-        Card.Value THREE = Card.Value.THREE;
-        Card.Value FOUR = Card.Value.FOUR;
-        Card.Value FIVE = Card.Value.FIVE;
-        Card.Value SIX = Card.Value.SIX;
-        Card.Value SEVEN = Card.Value.SEVEN;
-        Card.Value EIGHT = Card.Value.EIGHT;
-        Card.Value NINE = Card.Value.NINE;
-        Card.Value TEN = Card.Value.TEN;
-        Card.Value JACK = Card.Value.JACK;
-        Card.Value QUEEN = Card.Value.QUEEN;
-        Card.Value KING = Card.Value.KING;
 
         //B:
         switch (card) {
             case ACE:
-                scoreBoard.put(ACE, scoreBoard.get(ACE) + 1);
+                scoreBoard[0][1] += 1;
                 return scoreBoard;
             case TWO:
-                scoreBoard.put(TWO, scoreBoard.get(TWO) + 1);
+                scoreBoard[1][1] += 1;
                 return scoreBoard;
             case THREE:
-                scoreBoard.put(THREE, scoreBoard.get(THREE) + 1);
+                scoreBoard[2][1] += 1;
                 return scoreBoard;
             case FOUR:
-                scoreBoard.put(FOUR, scoreBoard.get(FOUR) + 1);
+                scoreBoard[3][1] += 1;
                 return scoreBoard;
             case FIVE:
-                scoreBoard.put(FIVE, scoreBoard.get(FIVE) + 1);
+                scoreBoard[4][1] += 1;
                 return scoreBoard;
             case SIX:
-                scoreBoard.put(SIX, scoreBoard.get(SIX) + 1);
+                scoreBoard[5][1] += 1;
                 return scoreBoard;
             case SEVEN:
-                scoreBoard.put(SEVEN, scoreBoard.get(SEVEN) + 1);
+                scoreBoard[6][1] += 1;
                 return scoreBoard;
             case EIGHT:
-                scoreBoard.put(EIGHT, scoreBoard.get(EIGHT) + 1);
+                scoreBoard[7][1] += 1;
                 return scoreBoard;
             case NINE:
-                scoreBoard.put(NINE, scoreBoard.get(NINE) + 1);
+                scoreBoard[8][1] += 1;
                 return scoreBoard;
             case TEN:
-                scoreBoard.put(TEN, scoreBoard.get(TEN) + 1);
+                scoreBoard[9][1] += 1;
                 return scoreBoard;
             case JACK:
-                scoreBoard.put(JACK, scoreBoard.get(JACK) + 1);
+                scoreBoard[10][1] += 1;
                 return scoreBoard;
             case QUEEN:
-                scoreBoard.put(QUEEN, scoreBoard.get(QUEEN) + 1);
+                scoreBoard[11][1] += 1;
                 return scoreBoard;
             case KING:
-                scoreBoard.put(KING, scoreBoard.get(KING) + 1);
+                scoreBoard[12][1] += 1;
                 return scoreBoard;
         }//End SW:*
         return scoreBoard;
     }//End M:*
 
     //Define: Calls record types on each card and ticks the matching tallies in scoreBoard
-    public HashMap<Card.Value, Integer> checkHand(HashMap<Card.Value, Integer> countHolder, ArrayList<Card> cardHand) {
+    public int[][] checkHand(int[][] countHolder, ArrayList<Card> cardHand) {
         for (int i = 0; i < cardHand.size(); i++) {
             recordTypes(cardHand.get(i).getValue(), countHolder);
         }//End F:*
@@ -192,7 +177,7 @@ public class Dealer {
 
     //Define: determine which card to ask for based on players hand having duplicates.
     public Card.Value[] cardDecision(
-            HashMap<Card.Value, Integer> scoreBoard,
+            int[][] scoreBoard,
             Card.Value[] dupesHolder
     ) {
 
@@ -200,8 +185,8 @@ public class Dealer {
         Card.Value[] values = Card.Value.values();
 
         //B: repeat. set a list of players duplicate values. 
-        for (int i = 0; i < values.length; i++) {
-            if (scoreBoard.get(values[i]) > 1) {
+        for (int i = 0; i < scoreBoard.length; i++) {
+            if (scoreBoard[i][1] > 1) {
                 dupesHolder[i] = values[i];
             }//End I:*
         }//End F:*
@@ -268,37 +253,26 @@ public class Dealer {
     //To simulate receiving the card from your opponent and
     //Putting it in your hand. 
     public ArrayList<Card> addCard(ArrayList<Card> cardHand, Card card) {
-
         cardHand.add(card);
         return cardHand;
     }//End M:*
 
-    //Takes as parameter a hash map with left key as card number and
-    //right value is just the 4 amount value to indicate a book was made. 
+  
     //Returns the players book
-    public HashMap<Integer, Integer> updateBooks(HashMap<Integer, Integer> books, int[][] scoreBoard) {
-        for (int i = 0; i < 13; i++) {
+    public int[][] updateBooks(int[][] books, int[][] scoreBoard) {
+        for (int i = 0; i < scoreBoard.length; i++) {
             if (scoreBoard[i][1] >= 4) {
-                books.put(scoreBoard[i][0], scoreBoard[i][1]);
-            }
-        }
+                books[i][1] = 1;
+            }//End I:*
+        }//End F:*
         return books;
-    }
+    }//End M:*
 
     //compares player objects field var books. 
     //Comparison is based on nunber of key value pairs in books 
-    public Player determineWinner(Player human,
-            Player computer,
-            HashMap<Card.Value, Integer> hBook,
-            HashMap<Card.Value, Integer> cBook,
-            Player winner) {
-        if (hBook.size() > cBook.size()) {
-            winner = human;
-        } //End I:*
-        else {
-            winner = computer;
-        }//End E:*
-        return winner;
+    public Player determineWinner() {
+      
+        return null;
     }//End M:*
 
     //This prompts for a card it knows about from a 

319e06fc29dc2f41aa64519261a93f4c8e0f532a
aleks <aleks@192.168.2.49>
2021-03-26 19:02:22 -0400

    Fixing how program starts.

---
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project.Start;
+
+/**
+ *
+ * @author aleks
+ */
+public class Main {
+    public static void main(String[] args){
+        RoundOne round1 = new RoundOne(new GoFish());
+        round1.play();
+    }//End M:*
+    
+}//End CL:*

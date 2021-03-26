6756f9148786ceb63f65a466adef8f32003b788e
aleks <aleks@192.168.2.49>
2021-03-26 17:08:07 -0400

    Final clean up.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/GoFish.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Start/GoFish.java
@@ -3,13 +3,13 @@ package ca.sheridancollege.project.Start;
 //Imports:__________________________
 
 import ca.sheridancollege.project.Cards.Card;
+import ca.sheridancollege.project.Cards.Hand;
 import ca.sheridancollege.project.Players.CompPlayer;
 import ca.sheridancollege.project.Players.HumanPlayer;
 import ca.sheridancollege.project.Players.Player;
-import ca.sheridancollege.project.Turns.Dealer;
-import ca.sheridancollege.project.Turns.Deck;
 import ca.sheridancollege.project.Turns.ScoreBoard;
 import ca.sheridancollege.project.Turns.TurnManager;
+import ca.sheridancollege.project.Utility.Printer;
 import java.util.ArrayList;
 
 
@@ -23,44 +23,42 @@ import java.util.ArrayList;
  ***************************************************************/
 public class GoFish extends Game {
 
-    private final Dealer dealer; 
     private final Player human;
     private final Player computer;
     private final TurnManager turnController;
     private final ScoreBoard scoreBoard;
+    private static GoFish game;
    
             
-    public GoFish(     String hName,
-                       ArrayList<Card> hand,
-                       ArrayList<Card> desirableList,
-                       String cName,
-                       TurnManager turnController) {
-        super("Go Fish");
-        this.dealer = new Dealer(new Deck(), new ArrayList<>());
+    public GoFish() {
         this.human = new HumanPlayer();
         this.computer = new CompPlayer();
-       this.turnController = turnController; 
+       this.turnController = new TurnManager(
+                                             new HumanPlayer(), 
+                                             new CompPlayer(),
+                                             new ArrayList<>(),
+                                             new ArrayList<>(),
+                                             new Hand()
+                                            ); 
        this.scoreBoard = new ScoreBoard();
+       GoFish.game = new GoFish();
+       
         
     }//End C:*
     public GoFish(
-                  Dealer dealer,
                   Player human,
                   Player computer,
                   TurnManager turnController
-                  ) {
-        super("Go Fish");
-        this.dealer = dealer;
+                  ) 
+    {
         this.human = human;
         this.computer = computer;
         this.turnController = turnController; 
         this.scoreBoard = new ScoreBoard();
+        GoFish.game = new GoFish();
 
     }//End C:*
 
-    public Dealer getDealer() {
-        return dealer;
-    }
 
     public Player getHuman() {
         return human;
@@ -78,16 +76,39 @@ public class GoFish extends Game {
     public void play() {
 
       //A: Setting up hands.
-      dealer.createHand(7);
-      human.setHand(dealer.getHand());
-      
-      dealer.createHand(7);
-      computer.setHand(dealer.getHand());
-      
+      //A.1: 
+      System.out.println("Creating and setting humans hand");
+      turnController.getHand().createHand(7);
+      human.setHand(turnController.getHand().getHand());
+      System.out.println("Printing humans hand");
+      Printer.printHand(human.getHand());
+      //A.2:
+      System.out.println("Creating and setting computers hand");
+      turnController.getHand().createHand(7);
+      computer.setHand(turnController.getHand().getHand());
+      System.out.println("Printing computers hand");
+      Printer.printHand(computer.getHand());
+
       //B: Setting desireable lists. 
+      //B.1: 
+      System.out.println("Setting humans desirable list");
       human.setDesirableList(scoreBoard.getDupes(human.getHand()));
-      computer.setDesirableList(scoreBoard.getDupes());
+      System.out.println("Printing humans desirable list");
+      Printer.printHand(human.getDesirableList());
       
+      //B.2:
+      System.out.println("Setting computers desirable list");
+      computer.setDesirableList(scoreBoard.getDupes());
+      System.out.println("Printing computers desirable list");
+      Printer.printHand(computer.getDesirableList());
+      
+      
+      
+    }//End M:*
+    
+    //Define: programs starting point.
+    public static void main(String[] args){
+        game.play();
     }//End M:*
   
     @Override

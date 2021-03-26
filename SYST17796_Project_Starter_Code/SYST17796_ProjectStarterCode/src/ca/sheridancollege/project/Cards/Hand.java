aeabf6f35d57a19ea518742847a9a2f7f7012deb
aleks <aleks@192.168.2.49>
2021-03-26 17:07:53 -0400

    Final clean up.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Cards/Hand.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Cards/Hand.java
@@ -6,6 +6,8 @@
 package ca.sheridancollege.project.Cards;
 
 import java.util.List;
+import ca.sheridancollege.project.Turns.Deck;
+import java.util.ArrayList;
 
 /**
  *
@@ -13,6 +15,26 @@ import java.util.List;
  */
 public class Hand {
     
+    private ArrayList<Card> hand;
+    private Deck deck;
+
+    public Hand(Deck deck) {
+        this.deck = deck;
+    }//End C:*
+
+    public Hand() {
+        this.deck = new Deck();
+    }//End C:*
+    
+    
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
     
     //Define: adds a Card to a provided hand and copies the updated hand.
     private List<Card> addCardToHand(List<Card> cardHand, Card card) {
@@ -75,24 +97,65 @@ public class Hand {
     }//End M:*
     
     //Define: behavior for taking random card from deck after told to go fish. 
-    private GoFishCard createRandoCard() 
+    private Card createRandoCard() 
     {
         //A: create: ranges for random number to map against. 
         Card.Value[] values = Card.Value.values();
-        Card.Suit[] suits = Card.Suit.values();
 
         //B: Create: the random numbers modelling suit and values. 
         int valPossible = (int) (Math.random() * values.length) + 1;
-        int suitPossible = (int) (Math.random() * suits.length) + 1;
 
         //C: asssign: the values determined by a random number
-        Card.Value randVal = values[valPossible];
-        Card.Suit randSuit = suits[suitPossible];
+        Card resCard = deck.getDeck().get(valPossible);
 
         //D: create: the card object and copy it. 
-        Card rCard = new GoFishCard(randSuit, randVal);
+        deck.removeCard(resCard);
         
-        return null;
+        return resCard;
     }//End M:*
+    
+    
+    //Define: normal getter for accessing the hand.
+    public ArrayList<Card> getHand() {
+        return hand;
+    }//End G:*
+
+    //Define: normal setter for a new hand.
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }//End S:*
+
+    //Methods:_________________________
+    
+    //Define: this fills deck and shuffles it. Uses 2 known methods.
+    private void deckSetup() {
+        deck.initDeck();
+        deck.shuffle();
+    }//End M:*
+
+    //Define: Deal first card and simultaneously remove it from deck.
+    private Card startDeal() {
+        Card card;
+        card = deck.getDeck().get(0);
+        //B: 
+        deck.getDeck().remove(0);
+        //C: 
+        return card;
+    }//End M:*
+
+    //Define: provide number of cards. Repetition solution. Call's start deal.
+    public void createHand(int size) {
+
+        if (hand.size() > 0) {
+            hand.clear();
+        }//End I:*
+
+        for (int i = 0; i < size; i++) {
+            hand.add(this.startDeal());
+        }//End F:*     
+
+    }//End M:*
+
+    
 
 }//End CL:*

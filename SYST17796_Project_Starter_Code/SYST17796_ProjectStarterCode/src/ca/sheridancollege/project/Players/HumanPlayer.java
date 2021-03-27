335282b28bd6eca0cc3a0e709c852155073ecd5f
aleks <aleks@192.168.2.49>
2021-03-27 14:56:57 -0400

    Problems with asking for cards of opponents and updating hands accordingly.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/HumanPlayer.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/HumanPlayer.java
@@ -15,8 +15,8 @@ package ca.sheridancollege.project.Players;
 public class HumanPlayer extends Player{
     
  
-    public HumanPlayer(){
-        super();
+    public HumanPlayer(String name){
+        super(name);
     }//End C:*
     
  

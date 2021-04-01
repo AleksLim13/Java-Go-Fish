1180f48da88bb7df9e8d9aa01d9630d42c9eec3e
aleks <aleks@192.168.2.49>
2021-04-01 14:46:22 -0400

    Changing parameter of methods to whole player object instead of piece meal passing.

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/CompPlayer.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Players/CompPlayer.java
@@ -7,14 +7,18 @@ package ca.sheridancollege.project.Players;
 
 //Imports:________________________________
 
+import ca.sheridancollege.project.Cards.Card;
+import java.util.List;
+
+
 /*
  *
  * @author AllyCat13 @ Sheridan High 2021. 
  */
 public class CompPlayer extends Player {
 
-    public CompPlayer() {
-        super("Computer");//For visual understanding.
+    public CompPlayer(List<Card> books, List<Card> dL) {
+        super("Computer", books, dL);//For visual understanding.
     }//End C:*
 
    

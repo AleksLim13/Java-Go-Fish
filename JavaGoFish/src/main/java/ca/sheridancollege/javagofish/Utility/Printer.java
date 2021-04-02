5c7dbc3805ca2f48cbfe569d33e81cb2098572dc
aleks <aleks@192.168.2.49>
2021-04-02 19:57:53 -0400

    Writing comments for Printer class.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Utility/Printer.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Utility/Printer.java
@@ -1,27 +1,35 @@
 package ca.sheridancollege.javagofish.Utility;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 //Imports:______________________
 import ca.sheridancollege.javagofish.Cards.Card;
 import java.util.List;
 
 /**
- *
+ * Class for printing Player lists to the console so the human player knows what's happening and why.
  * @author AllyCat13 @ Sheridan High 2021
  */
+
+
 public final class Printer {
 
-    public Printer() {
+    /**
+     * We don't need to create a instance of Printer because we have simple static display methods.
+     */
+    public Printer() 
+    {
 
     }//End C:*
 
-    //Define: general behavior for printing a list of Cards. Hand or deck or book.
-    public static void printHand(List<Card> tHand) {
-        for (Card c: tHand) {
+    /**
+     * Static method for looping through a list and printing each item.
+     * @param tHand Card list type.
+     */
+    public static void printHand(List<Card> tHand) 
+    {
+        for (Card c: tHand) 
+        {
             System.out.println(c.toString());
         }//End F:*
     }//End method 

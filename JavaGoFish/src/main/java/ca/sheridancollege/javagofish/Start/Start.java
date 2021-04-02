7fe0aa53353c9b0e050f55f1f6245ee05173b7a7
aleks <aleks@192.168.2.49>
2021-04-02 19:29:10 -0400

    Writing comments for Start class.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/Start.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/Start.java
@@ -1,12 +1,16 @@
 package ca.sheridancollege.javagofish.Start;
 
 /**
- *
- * @author aleks
+ * This class models how a Game starts and plays. 
+ * Each instance of this class will define the way that the particular game plays.
+ * @author AllyCat13 : Sheridan High 2021.
  */
 public abstract class Start {
      //Define: programs starting point.
     
+    /**
+     * This method is where the programs overall steps will be defined.
+     */
     public abstract void play();
     
  

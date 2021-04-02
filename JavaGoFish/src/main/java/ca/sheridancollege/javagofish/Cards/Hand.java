9072e12da018218d0479887a91c5ce3c6241e5b9
aleks <aleks@192.168.2.49>
2021-04-02 18:29:46 -0400

    Writing comments for hand class.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Cards/Hand.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Cards/Hand.java
@@ -9,15 +9,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * This class has all the functionality for tasks relating to a players hand.
+ * Only one of these Hand objects need to be created during a games life cycle. 
+ * It can be declared as a final field variable in the aggregating class. 
+ * This class aggregates Deck and Scoreboard as it needs one of each along with their functionality.
  * @author AllyCat13:
  *
  */
 public class Hand {
 
+    /**
+     * A reference to scoreboard is needed for calculating duplicates of players hand.
+     * Once a scoreboard object is created it's life cycle is complete.
+     * A Hand instance will access scoreboard through a public getter interface.
+     */
     private final ScoreBoard scoreBoard;
-    private Deck deck;
+    
+    /**
+     * A Hand instance needs a reference to a Deck instance to create hands and update the Deck. 
+     * Only one copy is ever needed and needs to be created only once.
+     */
+    private final Deck deck;
 
+    /**
+     * This constructs a Hand instance with a Deck and Scoreboard object as argument thereby initializing the field variables.
+     * @param deck is of Deck type. 
+     * @param sb is of ScoreBoard type. 
+     * This constructors calls this classes deck setup method to automatically initialize and shuffle the deck.
+     */
     public Hand(Deck deck, ScoreBoard sb) 
     {
         this.deck = deck;
@@ -25,43 +44,57 @@ public class Hand {
         this.deckSetup();
     }//End C:*
 
+    /**
+     * This is a public getter interface for accessing the Deck field var of Hand instances.
+     * @return the field variable of Deck type.
+     */
     public Deck getDeck() 
     {
         return this.deck;
     }//End G:*
 
-    public void setDeck(Deck deck) 
-    {
-        this.deck = deck;
-    }//End S:*
-
-    //Define: yes. the opponent has the card. add to inplay and subtract not in play.
+    
+    /**
+     * This method removes the Card asked for by the opponent from the asked players hand.
+     * This method adds the Card to the players hand who asked for it. 
+     * @param inPlay is of top level Player type.
+     * @param inPlaysDesireC is is of top level Card type. 
+     * @param notInPlay is of top level Player type. 
+     */
     private void addCardToHand(
             Player inPlay, //inplay
             Card inPlaysDesireC, //in plays desire
             Player notInPlay //remove from
     ) {
 
+        //A: Find: 
         int posit = findPositPartialCard(notInPlay, inPlaysDesireC);
+        //B: Get:
         Card cTemp = notInPlay.getHand().get(posit);
+        //C: Remove: 
         updateHandDelete(notInPlay, cTemp);
+        //D: Add: 
         inPlay.getHand().add(cTemp);
 
-        System.out.println("");
-        
-
+        //E: 
         System.out.println("");
         System.out.println("Calculating dupes for " + inPlay.getName());
+        //E.1: Calculate:
         this.scoreBoard.getDupes(inPlay);
-
         System.out.println("");
+        //E.2: Order: 
         this.sort(inPlay, 'd');
         System.out.println(inPlay.getName() + " dupes: ");
+        //E.3: Display: 
         Printer.printHand(inPlay.getDesirableList());
 
     }//End M:*
 
-    //Define: opponent has card. Remove it after giving it to player in play. 
+    /**
+     * This method removes the target Card from the Player's hand who is being asked.
+     * @param notInPlay is of top level Player type. 
+     * @param posit is of integer type representing the position of the target Card.
+     */
     private void deleteCardFromHand(
             Player notInPlay,
             int posit
@@ -70,7 +103,11 @@ public class Hand {
         notInPlay.getHand().remove(posit);
     }//End M:*
     
-    //Define: opponent has card. Remove it after giving it to player in play. 
+    /**
+     * The game has a need to remove a Card from duplicate list when the opponent asks for it and the asked person has it.
+     * @param notInPlay top level Player type.
+     * @param posit integer type. 
+     */
     private void deleteCardFromDList(
             Player notInPlay,
             int posit
@@ -79,8 +116,14 @@ public class Hand {
         notInPlay.getDesirableList().remove(posit);
     }//End M:*
 
-    //Define: "sorry don't have that card dude." Allor, take a card from the deck.  
-    public void getCardFromDeck(Player player) {
+    /**
+     * This method is for when a Players asks for a Card and the asked player doesn't have it
+     * That player who asked has to "Gi Fish" and draw a Card from the top of the deck. 
+     * This method calls another method from this class that chooses a value from a random slot in the deck. 
+     * @param player is of top level Player type. 
+     */  
+    public void getCardFromDeck(Player player) 
+    {
         player.getHand().add(createRandoCard());
         System.out.println("");
 
@@ -93,7 +136,12 @@ public class Hand {
         System.out.println(player.getName() + " dupes: " + player.getDesirableList().toString());
     }//End M:*
 
-    //Define: Not in play has the card. Remove it from not in play. 
+    /**
+     * This is the public interface for removing the Card from the asked players hand.
+     * It deletes from the Players duplicates list to keep hand and duplicate list in sync.
+     * @param notInPlay top level Player type.
+     * @param inPlaysDesireC top level Card type.
+     */
     public void updateHandDelete(
             Player notInPlay,//Incl: list for C's.
             Card inPlaysDesireC//Incl: list for copies. 
@@ -105,7 +153,13 @@ public class Hand {
         deleteCardFromHand(notInPlay, pHTemp);
     }//End M:*
 
-    //Define: ablility to remove target from other players hand. Calls delete method. 
+    /**
+     * public interface for adding a Card to the Player who asked for it if it's also possessed by asked player.
+     * This calls this classes private method add card to hand.
+     * @param inPlay top level Player type.
+     * @param inPlaysDesireC top level card type.
+     * @param notInPlay  top level Player type.
+     */
     public void updateHandAdd(
             Player inPlay,//Incl: list for C's.
             Card inPlaysDesireC,//Incl: list for copies. 
@@ -115,9 +169,12 @@ public class Hand {
         addCardToHand(inPlay, inPlaysDesireC, notInPlay);
     }//End M:*
 
-    //Need to know what the index of the card need to remove 
-    //from hand
-    //Define: 
+    /**
+     * This finds the index value of the target Card that matches by exact suit and value.
+     * @param player top level Player type.
+     * @param tCard top level Card type. 
+     * @return integer value as position in asked Players hand.
+     */
     private int findPositFullCard(Player player, Card tCard) 
     {
         int posit = 0;
@@ -134,7 +191,13 @@ public class Hand {
     
  
 
-    //Define: 
+    /**
+     * When the asked for Card is possessed by the asked player it needs to be remove from two places.
+     * Cards match by exact suit and value facilitated by the overridden equals method in Card.
+     * @param player top level Player type.
+     * @param tCard top level Card type. 
+     * @return integer as position of target card in searched hand. 
+     */
     private int findPositFullCardDList(Player player, Card tCard) 
     {
         int posit = 0;
@@ -149,7 +212,13 @@ public class Hand {
         return posit;
     }//End M:*
 
-    //Define: 
+    /**
+     * Players as for a Card by value only. Players ask by value and not by value and suit.
+     * The goal is to get as many four of a kinds as possible before the deck runs out of cards.
+     * @param player top level Player type.
+     * @param tCard top level Card type. 
+     * @return integer as position of target card in asked players hand.
+     */
     private int findPositPartialCard(Player player, Card tCard) 
     {
         int posit = 0;
@@ -163,7 +232,12 @@ public class Hand {
         }//End F:*
         return posit;
     }//End M:*
-    //Define: behavior for taking random card from deck after told to go fish. 
+    
+    /**
+     * To be used when a Player has to "Go Fish" because they asked for a Card the asked PLayer doesn't have.
+     * It created a random number that could be any slot in the Card list deck. 
+     * @return the random Card removed and deleted from the central deck. 
+     */
     private Card createRandoCard() 
     {
         //A: create: ranges for random number to map against. 
@@ -181,14 +255,22 @@ public class Hand {
     }//End M:*
 
     //Methods:_________________________
-    //Define: this fills deck and shuffles it. Uses 2 known methods.
+    
+    
+    /**
+     * This method performs routine tasks for the deck that occur once per game.
+     * It initializes the deck and shuffles it. 
+     */
     private void deckSetup() 
     {
         this.deck.initDeck();
         this.deck.shuffle();
     }//End M:*
 
-    //Define: Deal first card and simultaneously remove it from deck.
+    /**
+     * This method takes and deletes a Card from the central deck. 
+     * @return 
+     */
     private Card startDeal() 
     {//Notice: end of line style looks better.
         Card card;
@@ -199,16 +281,27 @@ public class Hand {
         return card;
     }//End M:*
 
-    //Define: provide number of cards. Repetition solution. Call's start deal.
+    /**
+     * Calls this classes start deal method and adds the return value per a desired amount of Cards to a Players hand.
+     * @param size as integer for how many cards to include in a Hand.
+     * @param player top level Player type. The Hand to add cards to. 
+     */
     public void createHand(int size, Player player) 
     {
         player.setHand(new ArrayList<>());
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) 
+        {
             player.getHand().add(this.startDeal());
         }//End F:*         
     }//End M:
 
+    /**
+     * Two Player lists need to be sorted throughout the game for Players to make quick decisions based on their hand.
+     * Sort either Players hand or duplicates list.
+     * @param player top level Player type. 
+     * @param option char type indicating which Player list to work on.
+     */
     public void sort(Player player, char option) 
     { 
         

d3202ef47816f94991d3720920b0979e204af742
aleks <aleks@192.168.2.49>
2021-04-09 00:52:00 -0400

    change deck and card class name to reflect that their abstract classes. Also, the IDE will line them up together because the same letter A is in front of the abstract classes name. 2 4 1 benefit.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Players/HumanPlayer.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Players/HumanPlayer.java
@@ -4,7 +4,7 @@ package ca.sheridancollege.javagofish.Players;
 
 //Imports:__________________________
 
-import ca.sheridancollege.javagofish.Cards.Card;
+import ca.sheridancollege.javagofish.Cards.ACard;
 import java.util.List;
 
 
@@ -22,7 +22,7 @@ public class HumanPlayer extends Player{
   * @param books card list type.
   * @param dL Card list type. 
   */
-    public HumanPlayer(String name, List<Card> books, List<Card> dL){
+    public HumanPlayer(String name, List<ACard> books, List<ACard> dL){
         super(name, books, dL);
     }//End C:*
     

260f49afb9b9de9890bbfc69335602379a1d04fa
aleks <aleks@192.168.2.49>
2021-04-09 02:01:55 -0400

    Renamed with adding a C because it's concrete.

---
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/AStart.java
@@ -0,0 +1,18 @@
+package ca.sheridancollege.javagofish.Start;
+
+/**
+ * This class models how a Game starts and plays. 
+ * Each instance of this class will define the way that the particular game plays.
+ * @author AllyCat13 : Sheridan High 2021.
+ */
+public abstract class AStart {
+     //Define: programs starting point.
+    
+    /**
+     * This method is where the programs overall steps will be defined.
+     */
+    public abstract void play();
+    
+ 
+    
+}//End CL:*

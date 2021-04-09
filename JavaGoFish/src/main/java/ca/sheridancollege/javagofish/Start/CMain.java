21fbf70dedf9dcd5d94f485f7f8447f0ba793227
aleks <aleks@192.168.2.49>
2021-04-09 02:02:34 -0400

    Renamed with adding a C because it's concrete.

---
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/CMain.java
@@ -0,0 +1,54 @@
+package ca.sheridancollege.javagofish.Start;
+
+import ca.sheridancollege.javagofish.Players.CCompPlayer;
+import ca.sheridancollege.javagofish.Players.CHumanPlayer;
+import ca.sheridancollege.javagofish.Cards.CGoFishDeck;
+import ca.sheridancollege.javagofish.Cards.CGoFishHand;
+import ca.sheridancollege.javagofish.Turns.CGoFishScoreBoard;
+import ca.sheridancollege.javagofish.Turns.TurnManager;
+import java.util.ArrayList;
+
+/**
+ * 
+ * The Main class with the main method for where the program starts.
+ * 
+ * 
+ * 
+ * 
+ * @author AllyCat13. 
+ */
+public class CMain 
+{
+    
+    /**
+     * The main method. Creates the RoundOne object. Calls the class RounOne play method.  
+     * @param args 
+     */
+    public static void main(String[] args)
+    {
+        TurnManager tM = new TurnManager(
+                                        new CHumanPlayer(
+                                                        "Aleks", 
+                                                        new ArrayList<>(), 
+                                                        new ArrayList<>()
+                                                         ),//End C:* 
+                                        new CCompPlayer(
+                                                       new ArrayList<>(), 
+                                                       new ArrayList<>()
+                                                       ), //End C:*
+                                        new CGoFishHand(
+                                                 new CGoFishDeck(new ArrayList<>()),
+                                                 new CGoFishScoreBoard()
+                                                ), //End C:*
+                                        new CGoFishScoreBoard()
+                                        );
+        
+        CRoundOne round1 = new CRoundOne(
+                                       new CGoFish(
+                                                  "Aleks", 
+                                                  tM)
+                                       );
+        round1.play();
+    }//End M:*
+    
+}//End CL:*

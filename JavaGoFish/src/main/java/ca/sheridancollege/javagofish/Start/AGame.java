c6b93afb0d5809a54935a3183d8f94383072a560
aleks <aleks@192.168.2.49>
2021-04-09 02:01:50 -0400

    Renamed with adding a C because it's concrete.

---
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/AGame.java
@@ -0,0 +1,20 @@
+package ca.sheridancollege.javagofish.Start;
+
+/**
+ * top level class for modeling generic card games.
+ * An instance like Black Jack or Big Two can extend this class. 
+ * @author AllyCat13 : Sheridan High 2021. 
+ */
+public abstract class AGame 
+{
+    
+    /**
+     * Basic constructor for creating a game example. 
+     */
+    public AGame(){
+   
+    }//End C:*
+    
+   
+    
+}//end class

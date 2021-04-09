0eb03357d91c9fbea61fb867005afddc872791ec
aleks <aleks@192.168.2.49>
2021-04-09 02:11:04 -0400

    Fixing comments.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Players/CCompPlayer.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Players/CCompPlayer.java
@@ -7,21 +7,28 @@ import java.util.List;
 
 
 /**
- * This a specific type of Player for modeling the computer.
+ * This a specific type of Player for modeling the computer. C is for concrete class.
  * This class inherits data and functionality from Player.
+ * 
+ * 
+ * 
  * @author AllyCat13 @ Sheridan High 2021. 
  */
-public class CCompPlayer extends APlayer {
+    public class CCompPlayer extends APlayer 
+
+{
 
     /**
      * Prevent null pointer exception with Card lists copies to initialize field variables. 
      * @param books Card list data type.
      * @param dL card list data type. 
      */
-    public CCompPlayer(List<ACard> books, List<ACard> dL) {
+        
+        
+    public CCompPlayer(List<ACard> books, List<ACard> dL) 
+    {
         super("Computer", books, dL);//For visual understanding.
     }//End C:*
 
-   
-    
-}//End Class:______________________+
+  
+}//End CL:*

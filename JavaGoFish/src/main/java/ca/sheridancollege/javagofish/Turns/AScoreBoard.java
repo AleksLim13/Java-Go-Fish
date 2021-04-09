266efe0c1e1122f48a685519de9ea93ee7405e3a
aleks <aleks@192.168.2.49>
2021-04-09 01:37:17 -0400

    Making ScoreBoard abstract and adding concrete class Go Fish Score Board.

---
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Turns/AScoreBoard.java
@@ -0,0 +1,80 @@
+
+package ca.sheridancollege.javagofish.Turns;
+
+
+/**
+ * This class contains the behavior for making calculations related to a Players hand.
+ * @author AllyCat13
+ */
+
+import ca.sheridancollege.javagofish.Cards.ACard;
+import ca.sheridancollege.javagofish.Players.Player;
+import java.util.List;
+
+
+public abstract class AScoreBoard 
+
+{
+    //Declare:
+    
+    /**
+     * This is a field variable for storing the winner of the game. 
+     */
+    protected Player winner;
+ 
+  
+    /**
+     * Construct a instance of ScoreBoard when the winner is known ahead of time. 
+     */
+    public AScoreBoard() 
+    {
+        
+    }//End C:*
+
+    public Player getWinner() 
+    {
+        return winner;
+    }//End G:*
+
+    public void setWinner(Player winner) 
+    {
+        this.winner = winner;
+    }//End S:*
+    
+    /**
+     * This method determines if a Player has a four of a kind and then records it.
+     * @param tNum top level Card type.
+     * @param player top level Player type. 
+     */
+    public abstract void calcBooks(
+            ACard tNum,
+            Player player
+    );
+     
+    /**
+     * This method determines where Cards are located that need to be worked on.
+     * @param tNum top level Card type.
+     * @param tList Card list type.
+     * @return integer as position of the Card in the target Hand.
+     */
+     public abstract int findFirstIndex(
+            ACard tNum,
+            List<ACard> tList
+     );
+      
+     /**
+      * This method creates a list of duplicates cards a Player has.
+      * @param player top level Player type. 
+      */
+    public abstract void getDupes(
+                              Player player
+                              ); 
+    /**
+     * 
+     * @param human top level Player type.
+     * @param computer top level Player type. 
+     * @return Player determined to be the one who made the most progress during the game.
+     */
+    public abstract Player determineWinner(Player human, Player computer); 
+    
+}//End CL:*

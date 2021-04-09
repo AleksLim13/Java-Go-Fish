be1e9db700a9ba9a8261ace57921950388008b84
aleks <aleks@192.168.2.49>
2021-04-09 02:00:01 -0400

    Renamed with adding a C because it's concrete.

---
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Players/CHumanPlayer.java
@@ -0,0 +1,30 @@
+
+
+package ca.sheridancollege.javagofish.Players;
+
+//Imports:__________________________
+
+import ca.sheridancollege.javagofish.Cards.ACard;
+import java.util.List;
+
+
+
+/**
+ *This models human players. 
+ * This class inherits data and functionality from Player. 
+ * @author alllyCat13 @ Sheridan High 2021
+ */
+public class CHumanPlayer extends APlayer{
+    
+ /**
+  * Constructs a Player and initializes name, books, and duplicate list.
+  * @param name of String type.
+  * @param books card list type.
+  * @param dL Card list type. 
+  */
+    public CHumanPlayer(String name, List<ACard> books, List<ACard> dL){
+        super(name, books, dL);
+    }//End C:*
+    
+ 
+}//End Class:__________________+

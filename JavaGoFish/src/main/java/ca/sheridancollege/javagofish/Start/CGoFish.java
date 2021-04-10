b18e31f30271b771e9eb9f477d1ff078bc48be7d
aleks <aleks@192.168.2.49>
2021-04-10 01:48:03 -0400

    Reference to tM should be in abstract class.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/CGoFish.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/CGoFish.java
@@ -7,11 +7,19 @@ import ca.sheridancollege.javagofish.Turns.ATurnManager;
 
 
 /**
+ * CGOFISH CONCRETE CLASS:
+ * -----------------------
+ *
  * Specific example of a Game. This is a Go Fish version of a Card game.
+ * 
+ * 
+ * 
  * @author AllyCat13 : Sheridan High 2021
  */
 
-public class CGoFish extends AGame {
+public class CGoFish extends AGame 
+
+{
 
    /**
     * Each instance of Go Fish needs a reference to TurnManager. 
@@ -19,7 +27,7 @@ public class CGoFish extends AGame {
     * TurnManager contains the functionality for asking for Cards and switching who's
     * being asked and who's asking. 
     */
-    private final ATurnManager turnController;
+    
    
     /**
      * Creates a instance of Go Fish and initializes game name and turn manager. 
@@ -34,28 +42,22 @@ public class CGoFish extends AGame {
      */
     public CGoFish(String hName, ATurnManager tN) 
     {
- 
-        this.turnController = tN;
-       
-        
+        super(tN);
     }//End C:*
     
     /**
      * Constructs a instance of Go Fish and initializes TurnManager.
-     * @param turnController TurnManager type.
+     * @param tM
+     * 
      */
     public CGoFish(
-                  ATurnManager turnController
+                  ATurnManager tM
                   ) 
     {
-        this.turnController = turnController; 
+        super(tM);
 
     }//End C:*
 
-  
-    public ATurnManager getTurnController() {
-        return turnController;
-    }
-
+ 
 }//End Class:________________+
 

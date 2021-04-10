917413226f29105b2179c6ae23de6fc252c4deea
aleks <aleks@192.168.2.49>
2021-04-10 01:34:12 -0400

    Fixing comments.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/AGame.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Start/AGame.java
@@ -1,17 +1,30 @@
 package ca.sheridancollege.javagofish.Start;
 
 /**
- * top level class for modeling generic card games.
- * An instance like Black Jack or Big Two can extend this class. 
+ * AGAME ABSTRACT CLASS:
+ * ---------------------
+ * 
+ * 
+ * A top level class for modeling generic card games. We'll subtype other Game objects from this class 
+ * making this a template then. We may have Black Jack and Rummy in the future. We could be asked to make 
+ * another card game and we don't want to start from scratch. So, we abstract common attributes among card
+ * games here. Not right now, that will be expanded on later. For now it's just a class we designated
+ * as the Game template. An instance like Black Jack or Big Two can extend this class. 
+ * 
+ * 
+ * 
  * @author AllyCat13 : Sheridan High 2021. 
  */
-public abstract class AGame 
+
+    public abstract class AGame 
+
 {
     
     /**
      * Basic constructor for creating a game example. 
      */
-    public AGame(){
+    public AGame()
+    {
    
     }//End C:*
     

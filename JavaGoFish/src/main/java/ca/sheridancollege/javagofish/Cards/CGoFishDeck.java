b3881961daced6801f354a36abbf3ae374e960e4
aleks <aleks@192.168.2.49>
2021-04-17 11:45:08 -0400

    Trying to get a good exception handling design down.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Cards/CGoFishDeck.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Cards/CGoFishDeck.java
@@ -9,17 +9,16 @@ import java.util.List;
 
 /**
  *
- * GOFISHDECK CONCRETE CLASS:
- * --------------------------
- * 
- * 
- * 
- * 
+ * GOFISHDECK CONCRETE CLASS: 
+ * -------------------------- 
+ * This is a concrete class. It's the implementation for ADeck. It is a example of ADeck. In this
+ * case it is a Go Fish Deck. I added some exception handling for best and worst case scenarios.
+ *
+ *
+ *
  * @author AllyCat13 @ Sheridan High 2021.
  */
-
-
-    public class CGoFishDeck extends ADeck
+   public class CGoFishDeck extends ADeck 
 
 {//Start CL:*
 
@@ -31,31 +30,47 @@ import java.util.List;
     @Override
     public void initDeck() 
     {
-        for (String suitsRange : ACard.suitsRange) 
+        try 
         {
-            for (String valuesRange : ACard.valuesRange) 
+            for (String suitsRange : ACard.suitsRange) 
             {
-                this.deck.add(new CGoFishCard(suitsRange, valuesRange));
-            } //End Inner F:*
-        } //end outter for   
+                for (String valuesRange : ACard.valuesRange) 
+                {
+                    this.deck.add(new CGoFishCard(suitsRange, valuesRange));
+                } //End Inner F:*
+            } //end outter for   
+        }//End TRY:*
+        catch (NullPointerException | ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("The deck field could be null or accessing the index failed " + e);
+        }//End CAT:*
+
     }//End M:*
 
     @Override
     public void shuffle() 
-    
     {
-        int rando;
-        for (int i = 0; i < this.deck.size(); i++) 
+        try 
         {
-            //A: Create: 
-            rando = (int) (Math.random() * this.deck.size());      
-            //B: Get:
-            ACard temp = this.deck.get(i);
-            //C: Set: 
-            this.deck.set(i, this.deck.get(rando));
-            this.deck.set(rando, temp);         
-        }//End Outer F:*
+            int rando;
+            for (int i = 0; i < this.deck.size(); i++) 
+            {
+                //A: Create: 
+                rando = (int) (Math.random() * this.deck.size());
+                //B: Get:
+                ACard temp = this.deck.get(i);
+                //C: Set: 
+                this.deck.set(i, this.deck.get(rando));
+                this.deck.set(rando, temp);
+            }//End Outer F:*
+        }
+        
+        catch (NullPointerException | ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Has the deck field being initialized. Are array counters set properly");
+            System.out.println(e);
+        }//End CAT:*
+
     }//End M:*
 
-    
 }//End CL:*

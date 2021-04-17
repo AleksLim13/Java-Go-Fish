a1689de024b8c07d4cc8fdba7ca4859ffc38ee48
aleks <aleks@192.168.2.49>
2021-04-17 11:48:40 -0400

    Trying to get a good exception handling design down.

--- JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Utility/Printer.java
+++ JavaGoFish/src/main/java/ca/sheridancollege/javagofish/Utility/Printer.java
@@ -1,21 +1,22 @@
 package ca.sheridancollege.javagofish.Utility;
 
-
-
 //Imports:______________________
 import ca.sheridancollege.javagofish.Cards.ACard;
 import java.util.List;
 
 /**
- * Class for printing Player lists to the console so the human player knows what's happening and why.
+ * Class for printing Player lists to the console so the human player knows
+ * what's happening and why.
+ *
  * @author AllyCat13 @ Sheridan High 2021
  */
+   public final class Printer 
 
-
-public final class Printer {
+{
 
     /**
-     * We don't need to create a instance of Printer because we have simple static display methods.
+     * We don't need to create a instance of Printer because we have simple
+     * static display methods.
      */
     public Printer() 
     {
@@ -24,14 +25,25 @@ public final class Printer {
 
     /**
      * Static method for looping through a list and printing each item.
+     *
      * @param tHand Card list type.
      */
     public static void printHand(List<ACard> tHand) 
     {
-        for (ACard c: tHand) 
+        try 
         {
-            System.out.println(c.toString());
-        }//End F:*
+            for (ACard c : tHand) 
+            {
+                System.out.println(c.toString());
+            }//End F:*
+        } 
+        catch (NullPointerException | IndexOutOfBoundsException e) 
+        {
+            System.out.println(e);
+            System.out.println("Has the hand as method argument being initialized?");
+            System.out.println("Is the loop traversing the array slots properly?");
+        }//End CAT:*
+
     }//End method 
 
 } //End class
